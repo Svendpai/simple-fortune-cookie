4f39e032c9c9949ef8bc3c5c3db98dd36deb9475
Jakra19 <jakob1303@hotmail.com>
2021-08-19 16:33:42 +0000

    Fixes workflow test + changes ip to localhost

--- ci/docker-compose-curl-test.sh
+++ ci/docker-compose-curl-test.sh
@@ -3,7 +3,7 @@ docker-compose up -d --remove-orphans
 
 for i in {1..20}
 do
-    code=$(curl -s -o /dev/null -w "%{http_code}" http://34.140.143.115:8080)
+    code=$(curl -s -o /dev/null -w "%{http_code}" localhost:8080)
     echo $code
 
     if [ $code == 200 ]
