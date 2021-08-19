942cb82a222d1cee7c7b8f1530ae080687cc8a57
Jakra19 <jakob1303@hotmail.com>
2021-08-19 16:16:47 +0000

    Adds curl test forworkflow

---
+++ ci/docker-compose-curl-test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+docker-compose up -d --remove-orphans
+
+for i in {1..20}
+do
+    code=$(curl -s -o /dev/null -w "%{http_code}" http://34.140.143.115:8080)
+    echo $code
+
+    if [ $code == 200 ]
+    then
+        #Page exists
+        echo "pages exists"
+        exit 0
+    fi
+    sleep 2
+done
+
+docker-compose down
